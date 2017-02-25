58a3fcc194066497256bb97bba4abb88ecaea1aa
byronhernandez <byr0378@gmail.com>
2017-02-25 16:15:13 -0600

    test

--- evgApi/Global.asax.cs
+++ evgApi/Global.asax.cs
@@ -18,6 +18,7 @@ namespace evgApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
         }
     }
 }
