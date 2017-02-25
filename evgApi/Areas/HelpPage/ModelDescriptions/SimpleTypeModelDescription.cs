57f84827c3c2275dd2023c3b654c3eabdecf43e8
byronhernandez <byr0378@gmail.com>
2017-02-25 11:21:54 -0600

    base

---
+++ evgApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace evgApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ evgApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="evgApi.WebApiApplication" Language="C#" %>
